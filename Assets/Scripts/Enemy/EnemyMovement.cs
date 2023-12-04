04706dec723930fa626530872a0fbf4c7bb04a5c
Akinara666 <70153988+Akinara666@users.noreply.github.com>
2023-12-04 18:01:54 +0300

    Добавил скрипт для перемещения врага в точку игрока

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
+    Transform player;
+    public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Перемещаем врага в позицию игрока
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
     }
 }

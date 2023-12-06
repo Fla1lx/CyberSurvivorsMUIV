232065490bf8917aa18704533300726b8b1acb3b
Akinara666 <70153988+Akinara666@users.noreply.github.com>
2023-12-06 03:38:41 +0300

    Сделал нанесение урона с помощью GarlicWeapon

--- Assets/Scripts/Weapons/WeaponBase/MeleeWeaponBehavior.cs
+++ Assets/Scripts/Weapons/WeaponBase/MeleeWeaponBehavior.cs
@@ -8,10 +8,30 @@ public class MeleeWeaponBehavior : MonoBehaviour
     public WeaponScriptableObject weaponData;
 
     public float destroyAfterSeconds;
-    // Start is called before the first frame update
+    protected float currentDamage;
+    protected float currentSpeed;
+    protected float currentCooldownDuration;
+    protected int currentPierce;
+
+    void Awake()
+    {
+        currentDamage = weaponData.Damage;
+        currentSpeed = weaponData.Speed;
+        currentCooldownDuration = weaponData.CooldownDuration;
+        currentPierce = weaponData.Pierece;
+    }
     protected virtual void Start()
     {
         Destroy(gameObject, destroyAfterSeconds);
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Enemy"))
+        {
+            EnemyStats enemy = col.GetComponent<EnemyStats>();
+            enemy.TakeDamage(currentDamage);
+        }
     }
 
 

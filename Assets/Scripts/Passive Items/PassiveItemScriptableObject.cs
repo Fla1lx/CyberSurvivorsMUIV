f5054198234f04bf816d43431fb684efdb8e5bdb
Fla1lx <flailx@outlook.com>
2024-03-05 20:36:14 +0300

    Leveling Up PassiveItems and Weapons

--- Assets/Scripts/Passive Items/PassiveItemScriptableObject.cs
+++ Assets/Scripts/Passive Items/PassiveItemScriptableObject.cs
@@ -8,15 +8,15 @@ public class PassiveItemScriptableObject : ScriptableObject
     [SerializeField]
     float multipler;
     public float Multipler { get => multipler; private set => multipler = value; }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    [SerializeField]
+    int level;
+
+    public int Level { get => level; private set => level = value; }
+
+    [SerializeField]
+    GameObject nextLevelPrefab;
+
+    public GameObject NextLevelPrefab { get => nextLevelPrefab; private set => nextLevelPrefab = value; }
 }
